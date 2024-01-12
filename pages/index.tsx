8680bd5f41b9ff0f78bffb39998c8342945fcdd5
David Gaitsgory <dgaitsgo@student.42.fr>
2024-01-12 13:25:08 +0100

    hi

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,7 @@
 import React from "react"
-export default () =>
-	<div style={{background: '#002fa7', height: '100vh', width: '100vw' }}></div>
+function _() {
+	return <div style={{background: '#002fa7', height: '100vh', width: '100vw' }}></div>
+}
+
+export default _
+	
