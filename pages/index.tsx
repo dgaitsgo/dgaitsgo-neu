47c3d3e4206069c512a2a69cc3f8e43d48aea7d3
David Gaitsgory <dgaitsgo@student.42.fr>
2024-01-30 17:21:40 +0100

    only blue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,37 @@
 import React from "react"
+import { JetBrains_Mono } from '@next/font/google'
+
+const googleFont = JetBrains_Mono({
+	weight: '400',
+	subsets: ['latin'],
+})
+
 function _() {
-	return <div style={{background: '#002fa7', height: '100vh', width: '100vw' }}></div>
+
+	return (
+		<div className={`${googleFont.className} main`}>
+			<h1 className="header">Coming soon</h1>
+			<div className="countdown">
+				<p className="cdg">
+					<span className="label">days</span>
+					<span className="value">30</span>
+				</p>
+				<p className="cdg">
+					<span className="label">hours</span>
+					<span className="value">12</span>
+				</p>
+				<p className="cdg">
+					<span className="label">minutes</span>
+					<span className="value">44</span>
+				</p>
+				<p className="cdg">
+					<span className="label">seconds</span>
+					<span className="value">39</span>
+				</p>
+			</div>
+			<div id="email">copy email</div>
+		</div>
+	)
 }
 
 export default _
-	
