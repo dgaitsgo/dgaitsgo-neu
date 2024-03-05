a9bd6327f146c065cfded1d2c30d4bc5c5c29f4d
David Gaitsgory <dgaitsgo@student.42.fr>
2024-03-05 11:36:22 +0100

    global

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,55 @@ const googleFont = JetBrains_Mono({
 
 const countDownDate = new Date("May 16, 2091 16:20:25").getTime();
 
+const work = [
+	{
+		title: 'Silversea',
+		desc: 'Tablet app and design system for luxury cruise market leader.',
+		tags: ['Angular', 'C#', '.NET', 'SASS']
+	},
+	{
+		title: 'UpwardFacingCat',
+		desc: 'SaaS for dance instructors to manage classes',
+		tags: ['React', 'Next', 'PostgreSQL', 'AWS', 'TypeScript', 'Playwright']
+	},
+	{
+		title: 'Styleep',
+		desc: 'SaaS matching hair-stylists and salons',
+		tags: ['React', 'Node', 'PostgreSQL']
+	},
+	{
+		title: 'Pioneer.buzz',
+		desc: 'Developing an automated tool to convert raw tables into dynamic visualizations of marketing benchmarks in the cannabis industry.',
+		tags: ['python', 'D3', 'HTML/CSS']
+		
+	},
+	{
+		title: 'WiSight',
+		desc: 'UI for a security, knowledge base and asset management mobile application for French airports.',
+		tags: ['ReactNative', 'Typescript', 'xcode']
+	},
+	{
+		title: 'memetic influence',
+		desc: 'Created interactive visualizations for an analysis of meme propagation by Russian misinformation campaigns during the 2018 U.S. presidential election.',
+		tags: ['D3', 'HTML/CSS', 'Canvas'],
+	},
+	{
+		title: 'Public Health Post',
+		desc: 'Created custom data visualizations using a broad toolset to effectively communicate public health research to a larger audience.',
+		tags: ['D3', 'python', 'Jupyter notebooks', 'QGIS', 'Adobe Suite'],
+	},
+	{
+		title: 'Health Gorilla',
+		desc: 'Implemented user interfaces and interactive, novel data visualizations for enterprise health care applications servicing large hospitals and medical centers.',
+		tags: ['D3', 'React', 'MongoDB', 'heroku'],
+	},
+	{
+		title: 'Appworks Inc.',
+		desc: 'Built web applications from requirements to delivery. Business analysis, wire-framing for mobile and web applications, interactive data dashboards and structuring Restful APIs.',
+		tags: ['D3', 'React', 'ReactNative', 'node', 'PostgreSQL', 'Jenkins']
+	}
+]
+
 function getTimes() {
 	const now = new Date().getTime()
 
@@ -56,55 +105,40 @@ function _() {
 	}
 
 	return (
-		<div className={`${googleFont.className} main`}>
-			<h1 className="header">Full-stack Developer</h1>
-			<h2 className="header">David Gaitsgory</h2>
-			<div className="countdown">
-				<p className="cdg">
-					<span className="label">days</span>
-					<span className="value">{days}</span>
+		<div className='main'>
+			<div className='header'>
+			<h1>David Gaitsgory</h1>
+				<h2>Full-stack developer</h2>
+				<p>
+					4 years of experience in full stack development from concept to application.	
 				</p>
-				<p className="cdg">
-					<span className="label">hours</span>
-					<span className="value">{hours}</span>
-				</p>
-				<p className="cdg">
-					<span className="label">minutes</span>
-					<span className="value">{minutes}</span>
-				</p>
-				<p className="cdg">
-					<span className="label">seconds</span>
-					<span>
-						<motion.span key={'second0' + secondsStr[0]}
-							animate={{ opacity: 1 }}
-							initial={{
-								opacity: 0,
-							}}
-							transition={{
-								type: "spring",
-								stiffness: 100,
-								duration: 200
-							}}
-							className="value">{secondsStr[0]}
-						</motion.span>
-						<motion.span key={'seconds1' + secondsStr[1]}
-							animate={{ opacity: 1 }}
-							initial={{
-								opacity: 0,
-							}}
-							transition={{
-								ease: "linear",
-								duration: 0.2,
-								// type:,
-								// stiffness: 100,
-								// duration: 1000
-							}}
-							className="value">{secondsStr[1]}
-						</motion.span>
-					</span>
-				</p>
+				{/* <div id='email'>copy email</div> */}
 			</div>
-			<div id="email">copy email</div>
+			<div className='section'>
+				<h3 className='section-header'>WORK</h3>
+				<div className='projects'>
+					{
+						work.map((w) => {
+							return (
+								<div className='project-card'>
+									<h4 className='project-card__title'>{w.title}</h4>
+									<p>{w.desc}</p>
+									<div className="project-card__tags">
+										{w.tags?.map((tag) => {
+											return (
+												<span className="project-card__tag">{tag}</span>
+											)
+										})}
+									</div>
+								</div>
+							)
+						})
+					}
+				</div>
+			</div>
+			<div className='section'>
+				<h3 className='section-header'>Projects</h3>
+			</div>
 		</div>
 	)
 }
